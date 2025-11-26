f9db1df1737aa6d829c3d878446cdb2aa0891e73
masgor10 <bosokersgaming@gmail.com>
2025-11-27 02:27:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Tombol WhatsApp
+document.getElementById("waLink").onclick = function () {
+  window.open("https://wa.link/g072hb");
+};
+
+// Tombol pembayaran crypto
+document.getElementById("cryptoBtn").onclick = function () {
+  window.open("https://portfolio.metamask.io/transfer");
+};
